1dcb140541faddc821fbceafa597546c03a638f9
Sachin Sharma <sachin2252sharma@gmail.com>
2025-07-31 01:36:51 +0530

    API URL changes

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,7 @@
 export default {
   api: {
-    baseURL: 'http://ec2-43-204-149-38.ap-south-1.compute.amazonaws.com:3000/api',
+    // baseURL: 'http://ec2-43-204-149-38.ap-south-1.compute.amazonaws.com:3000/api',
+    baseURL: 'https://skillbloom-dev.onrender.com/api'
   },
   skillbloom_portfoilo_url: 'https://skillbloom-portfolio.vercel.app',
   razorpay: {
